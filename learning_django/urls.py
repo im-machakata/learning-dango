e29d1c746411f7e74055a79583c41484c7f8a326
Isaac Machakata <isaac@caasi.co.zw>
2023-03-29 05:37:04 +0200

    refactor: :}

--- learning_django/urls.py
+++ learning_django/urls.py
@@ -1,4 +1,4 @@
-"""wedding_invites URL Configuration
+"""learning_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
